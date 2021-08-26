fb03c7c9cd2e228cc2c64ea18049c89915367691
acoollevel <56624622+acoollevel@users.noreply.github.com>
2021-08-27 11:01:12 +1200

    Fixed some styling issues

--- navigation.js
+++ navigation.js
@@ -2,21 +2,10 @@ const render = path => {
     page = path.replace("#", "");
 
     if (page == "") {
-        page = "proposal";
+        page = "home";
     }
     document.querySelector("iframe").src = "pages/" + page + ".html";
 };
 
 window.onhashchange = evt => render(window.location.hash);
 render(window.location.hash);
-
-
-function resizeIFrameToFitContent( iFrame ) {
-    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
-    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-}
-
-window.addEventListener('DOMContentLoaded', function(e) {
-    var iFrame = document.querySelector("iframe");
-    resizeIFrameToFitContent( iFrame );
-} );
