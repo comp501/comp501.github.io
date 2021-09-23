d209d232ebfae9b5b76f9e95fcfd2f7a2f6705dc
acoollevel <56624622+acoollevel@users.noreply.github.com>
2021-09-24 10:20:36 +1200

    Highlight active nav item

--- navigation.js
+++ navigation.js
@@ -4,7 +4,12 @@ const render = path => {
     if (page == "") {
         page = "home";
     }
+    // load page
     document.querySelector("iframe").src = "pages/" + page + ".html";
+
+    // highlight active nav item
+    try {document.querySelector(`.active`).classList.remove("active")} catch {};
+    document.querySelector(`a[href="#${page}"]`).classList.add("active");
 };
 
 window.onhashchange = evt => render(window.location.hash);
